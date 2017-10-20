3878a867dfbef7944f5ba06156d2b67ff87e0009
Rafael Rivera <rafael@withinwindows.com>
2017-10-19 23:42:22 -0700

    Initial commit

---
+++ MainPage.xaml.cs
@@ -0,0 +1,18 @@
+﻿using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace DragDropWobblyRepro
+{
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private void Border_DragOver(object sender, DragEventArgs e)
+        {
+            e.AcceptedOperation = Windows.ApplicationModel.DataTransfer.DataPackageOperation.Link;
+        }
+    }
+}
